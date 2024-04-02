0e163ff4b028f4fe9eb269efbe1154575e28b3dd
JasrajChouhan <jasrajc945@gmail.com>
2024-04-02 11:38:17 +0530

    delete unnesscery things in build cmd

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import userRoute from './route/user.route.js'
 import { ErrorMiddleware } from "./middlewares/error.js";
 import contactUsRouter from './route/contactUs.route.js'
 import blogRouter from './route/blog.route.js'
-import path from 'path'
-import express from 'express'
+
 import cloudinary from  'cloudinary';
 import dotenv from 'dotenv'
 dotenv.config();
@@ -26,12 +25,8 @@ app.use('/api/v1/user' , userRoute);
 app.use('/api/v1/contactus' , contactUsRouter)
 app.use('/api/v1/blog' , blogRouter)
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname , '/client/dist')));
 
-app.get('*' , (req, res) => {
-    res.sendFile(path.join(__dirname , 'client' , 'dist' , 'index.html' ))
-})
+
 
 
 
